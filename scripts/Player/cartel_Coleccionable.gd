d60a5fc81b1dbfba7c5d0b35c6eec5f984752e1e
Karen <karenceles10@gmail.com>
2024-11-02 00:22:23 -0300

    changes in scripts

--- scripts/Player/cartel_Coleccionable.gd
+++ scripts/Player/cartel_Coleccionable.gd
@@ -1,7 +1,7 @@
 extends Label
-
+ 
 func _process(delta):
     $".".text="= "+str(Global.coleccionable)
-
+    
 func _on_BtnBack_pressed():
     get_tree().change_scene("res://Scenes/Menu/ChooseLvl.tscn")
