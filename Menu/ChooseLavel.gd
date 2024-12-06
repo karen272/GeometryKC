6a6fc6e0560cbf7eb0b350069923f98e027c8fb4
Karen <karenceles10@gmail.com>
2024-12-06 17:30:57 -0300

    Add btns from levels

--- Menu/ChooseLavel.gd
+++ Menu/ChooseLavel.gd
@@ -16,3 +16,13 @@ func _on_BtnLvl2_pressed():
 func _on_BtnBack_pressed():
     get_tree().change_scene("res://Menu/mundo.tscn")
     pass 
+
+
+func _on_BtnLvl3_pressed():
+    get_tree().change_scene("res://Levels/Lvl3.tscn")
+    pass 
+
+
+func _on_BtnLvl4_pressed():
+    get_tree().change_scene("res://Levels/Lvl4.tscn")
+    pass 
